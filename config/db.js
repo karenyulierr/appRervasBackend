c0234a4c3b06aba5bffd58f0bf25473bdbf93265
Karen Rodriguez <kyrodriguez@gmail.com>
2023-10-17 14:47:52 -0500

    Conectando a MongoDB

--- config/db.js
+++ config/db.js
@@ -1,19 +1,21 @@
 import mongoose from 'mongoose';
+import colors from 'colors';
 
 export const db = async () => {
     try {
-        const db = await mongoose.connect('mongodb+srv://root:ptwgG47d8FOVPJNq@cluster0.h5lqkds.mongodb.net/?retryWrites=true&w=majority')
-        console.log("se conecto correctamente ")
+        const db = await mongoose.connect(process.env.MONGO_URI)
+        const url = `${db.connection.host}:${db.connection.port}`
+        console.log(colors.cyan("MongoDB se conectó correctamente :", url))
     } catch (error) {
         console.log(`Error ${error.message}`)
         process.exit(1);
     }
 }
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const Cat = mongoose.model('Cat', { name: String });
+// const Cat = mongoose.model('Cat', { name: String });
 
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
 import { db } from './config/db.js';
 import servicesRoute from './routes/ServicesRoute.js';
 
+//Variables de entorno
+dotenv.config()
 //Confiugurar la app
 const app = express();
 
 //Conectar a bd
-
 db();
 
 //Definir Ruta
@@ -19,5 +22,5 @@ const PORT = process.env.PORT || 4000
 
 //Arrancar la app
 app.listen(PORT, () => {
-    console.log("El servidor se esta ejecutando en el puerto: ", PORT)
+    console.log(colors.blue("El servidor se esta ejecutando en el puerto:"), colors.blue.bold(PORT))
 })
