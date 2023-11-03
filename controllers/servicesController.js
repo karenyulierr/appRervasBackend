3b77afd2ec4e50be9cf5a79834a383aabbc64b76
Karen Rodriguez <kyrodriguez@gmail.com>
2023-11-03 14:46:16 -0500

    Finalizando servicios del backend

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -1,6 +1,4 @@
-import { services } from '../data/beautyServices.js';
 import Services from '../models/Services.js';
-
 import { validateObjectId, handleNotFoundError } from '../utils/index.js';
 
 const createService = async (req, res) => {
@@ -21,8 +19,13 @@ const createService = async (req, res) => {
     }
 }
 
-const getServices = (req, res) => {
-    res.json(services);
+const getServices = async (req, res) => {
+    try {
+        const services = await Services.find();
+        res.json(services);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const getServicesById = async (req, res) => {
--- routes/ServicesRoute.js
+++ routes/ServicesRoute.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import { createService, getServices, getServicesById, updateService,deleteService } from '../controllers/servicesController.js';
+import { createService, getServices, getServicesById, updateService, deleteService } from '../controllers/servicesController.js';
 
 const router = express.Router();
 
-router.post('/', createService)
-router.get('/', getServices)
-router.get('/:id', getServicesById)
-router.put('/:id', updateService)
-router.delete('/:id', deleteService)
+router.route('/')
+    .post(createService)
+    .get(getServices)
+
+
+router.route('/:id')
+    .get(getServicesById)
+    .put(updateService)
+    .delete(deleteService)
 
 export default router
